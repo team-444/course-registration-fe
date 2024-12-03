eac4bfd6f42f62b1829623f74f410ca722dc1dc1
junghanbyeol <gksquf9720@naver.com>
2024-12-03 15:54:14 +0900

    test init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
-
+import styled from "styled-components";
 function App() {
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
   return (
-    <div>hello</div>
+    <Container>
+      <H1>hello</H1>
+    </Container>
   );
 }
 
---
+++ frontend/src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const theme: DefaultTheme = {
+  bgColor: "red",
+  textColor: "black",
+  btnColor: "tomato"
+};
